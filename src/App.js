6801ac8d32c2507f1b9045eba0e5949266aedbc4
HenrifyPF38B <henrifyb@gmail.com>
2023-07-11 04:18:41 -0300

    Nacho

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
+import Router from './Router';
 
 function App() {
   return (
-    <div className="App">
-      <h2>Henrify 38B PF</h2>
-    </div>
+    <Router/>
   );
 }
 
