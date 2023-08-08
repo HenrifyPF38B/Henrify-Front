914f0d5f73b5d43bbcfe058bdf4632eb917df2f4
Arivera <alvaroriverafernandez@gmail.com>
2023-08-08 16:19:37 -0400

    Cambios diseño de Song

--- src/components/Admin Dashboard/SongDetail/SongDetail.jsx
+++ src/components/Admin Dashboard/SongDetail/SongDetail.jsx
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import { getSongsById } from '../../../redux/Actions/SongsActions'
+import { GiLoveSong } from 'react-icons/gi'
 import style from './SongDetail.module.css'
-
 
 const SongDetail = () => {
   const { id } = useParams()
@@ -33,22 +33,45 @@ const SongDetail = () => {
   console.log(songsId)
   return (
     <div className={style.songContainer}>
-      <h2>Song Detail</h2>
+      <div className={style.containerBtn}>
+        <h1 className={style.songDetail}>
+          <GiLoveSong /> Song Detail
+        </h1>
+        <div
+          style={{
+            border: songsId.deleted ? 'solid 3px red' : 'solid 3px green',
+            padding: '10px',
+            color: songsId.deleted ? 'red' : 'green',
+            fontWeight: 'bold',
+          }}
+        >
+          {!songsId.deleted ? 'Song enabled' : 'Song disabled'}
+        </div>
+      </div>
       {songsId && (
         <div className={style.song}>
-          
-          <img src={songsId.image} alt={songsId.name} className={style.imgSong}/>
-          <h1>{songsId.name}</h1>
-          <p>
-            Artists:
-            {songsId.artists.map((artist) => artist.name).join(', ')}
-          </p>
-          <p>Explicit: {songsId.explicit ? 'Yes' : 'No'}</p>
-          <p>Popularity: {songsId.popularity}</p>
-          <audio controls>
-            <source src={songsId.audioFull} type="audio/mpeg" />
-            Your browser does not support the audio element.
-          </audio>
+          <div className={style.nameSong}>
+            <h1>{songsId.name}</h1>
+            <img
+              src={songsId.image}
+              alt={songsId.name}
+              className={style.imgSong}
+            />
+          </div>
+          <div className={style.details}>
+            <div className={style.artistDetail}>
+              <h6>
+                <b>Artists: </b>
+                {songsId.artists.map((artist) => artist.name).join(', ')}
+              </h6>
+              <h6><b>Explicit: </b>{songsId.explicit ? 'Yes' : 'No'}</h6>
+            </div>
+            <h6><b>Popularity: </b>{songsId.popularity}</h6>
+            <audio controls>
+              <source src={songsId.audioFull} type="audio/mpeg" />
+              Your browser does not support the audio element.
+            </audio>
+          </div>
         </div>
       )}
     </div>
